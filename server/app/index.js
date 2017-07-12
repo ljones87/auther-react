68589aba463ff6c774e22c3b7fa789e2b9fd7c99
Lina Jones <lina.jones630@gmail.com>
2017-07-12 10:05:49 -0600

    counting

--- server/app/index.js
+++ server/app/index.js
@@ -2,6 +2,8 @@
 
 var app = require('express')();
 var path = require('path');
+var session = require('express-session');
+
 
 // "Enhancing" middleware (does not send response, server-side effects only)
 
@@ -10,7 +12,22 @@ app.use(require('./logging.middleware'));
 app.use(require('./body-parsing.middleware'));
 
 
+
 // "Responding" middleware (may send a response back to client)
+
+app.use(session({
+  // this mandatory configuration ensures that session IDs are not predictable
+  secret: 'tongiscool', // or whatever you like
+  // these options are recommended and reduce session concurrency issues
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use('/api', function (req, res, next) {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('*****counter****', ++req.session.counter);
+  next();
+});
 
 app.use('/api', require('../api/api.router'));
 
@@ -21,6 +38,9 @@ validFrontendRoutes.forEach(function (stateRoute) {
     res.sendFile(indexPath);
   });
 });
+
+
+
 
 app.use(require('./statics.middleware'));
 
